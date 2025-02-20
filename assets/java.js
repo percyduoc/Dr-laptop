36251c19f92762e80b7343d977f80973b58a3541
percyduoc <106414818+percyduoc@users.noreply.github.com>
2025-02-20 15:39:59 -0300

    2.11

--- assets/java.js
+++ assets/java.js
@@ -96,22 +96,6 @@ function revealServices() {
     });
 }
 window.addEventListener('scroll', revealServices);
-// nombre escribriendo 
-
-const text = "Dr Laptop";
-const speed = 100; // milliseconds per character
-let i = 0;
-
-function typeWriter() {
-    if (i < text.length) {
-        document.getElementById("text").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
-}
-
-window.onload = typeWriter;
-// 
 
 
 // 
